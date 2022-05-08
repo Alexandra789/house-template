0189da98ec0ba36a41d5500b2fc007169108316a
Alexandra1234 <alexandra.zakovinko>
2022-05-08 13:41:12 +0300

    fix adaptive header

--- src/js/headerSlider.js
+++ src/js/headerSlider.js
@@ -1,9 +1,6 @@
 import Swiper from 'swiper';
 
-console.log('================================');
-
 let swiper = new Swiper(".swiper", {
-    // slidesPerView: 3,
     spaceBetween: 16,
     slidesPerView: "auto",
     pagination: {
