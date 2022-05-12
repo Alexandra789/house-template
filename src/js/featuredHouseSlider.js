6cb90fdc4ab21ac3c8a3beb221a972b2ad7cf8ba
Alexandra1234 <alexandra.zakovinko>
2022-05-12 13:54:21 +0300

    add featured house block

--- src/js/featuredHouseSlider.js
+++ src/js/featuredHouseSlider.js
@@ -1,5 +1,3 @@
-import Swiper from 'swiper';
-
 let swiper = new Swiper(".featured-house__slider", {
     spaceBetween: 40,
     slidesPerView: "auto",
@@ -8,3 +6,40 @@ let swiper = new Swiper(".featured-house__slider", {
         prevEl: ".featured-house__slider-navigation-button-prev",
     },
 });
+
+$(".featured-house__filters .featured-house__filters__item").on("click", function () {
+    let filter = $(this).html().split('svg')[3].replace(/\s/g, '').slice(1);
+    let slidesxcol;
+    if (filter === "House") {
+        $("[data-filter]").removeClass("non-swiper-slide").addClass("swiper-slide").show();
+        if ($(".swiper-slide").length > 6)
+            slidesxcol = 3;
+        else slidesxcol = 1;
+        swiper.destroy();
+        swiper = new Swiper(".featured-house__slider", {
+            spaceBetween: 40,
+            slidesPerView: "auto",
+            slidesPerColumn: slidesxcol,
+            navigation: {
+                nextEl: ".featured-house__slider-navigation-button-next",
+                prevEl: ".featured-house__slider-navigation-button-prev",
+            },
+        });
+    } else {
+        $(".swiper-slide").not("[data-filter='" + filter + "']").addClass("non-swiper-slide").removeClass("swiper-slide").hide();
+        $("[data-filter='" + filter + "']").removeClass("non-swiper-slide").addClass("swiper-slide").attr("style", null).show();
+        if ($(".swiper-slide").length > 6)
+            slidesxcol = 3;
+        else slidesxcol = 1;
+        swiper.destroy();
+        swiper = new Swiper(".featured-house__slider", {
+            spaceBetween: 40,
+            slidesPerView: "auto",
+            slidesPerColumn: slidesxcol,
+            navigation: {
+                nextEl: ".featured-house__slider-navigation-button-next",
+                prevEl: ".featured-house__slider-navigation-button-prev",
+            },
+        });
+    }
+})
--- src/js/headerSlider.js
+++ src/js/headerSlider.js
@@ -1,6 +1,4 @@
-import Swiper from 'swiper';
-
-let swiper = new Swiper(".header__slider", {
+let swiperHeader = new Swiper(".header__slider", {
     spaceBetween: 16,
     slidesPerView: "auto",
     pagination: {
